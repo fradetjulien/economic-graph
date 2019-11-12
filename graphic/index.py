811f0248d5ce8e3cd2a6b2b2957a7aa01a36e9c6
Julien <julien.fradet5@gmail.com>
2019-11-12 17:00:25 -0500

    Functions name with _ and not camelCase

--- graphic/index.py
+++ graphic/index.py
@@ -4,7 +4,7 @@ import click
 import csv
 
 # Check if the file received in parameter is a correct CSV file
-def isCSV(file):
+def is_CSV(file):
     if not file.endswith('.csv'):
         print("Insert a correct CSV file please.")
         return
@@ -19,7 +19,7 @@ def isCSV(file):
     return
 
 # Check if the row correspond to the Equilibrium Price and Quantity
-def setEquilibrium(data, quantityDemanded, quantitySupply, price):
+def set_equilibrium(data, quantityDemanded, quantitySupply, price):
     if (quantityDemanded.isdecimal() and quantitySupply.isdecimal() and price.isdecimal() and
         (int(quantityDemanded) == int(quantitySupply))):
         data["equilibriumPrice"] = price
@@ -28,7 +28,7 @@ def setEquilibrium(data, quantityDemanded, quantitySupply, price):
     return data
 
 # Set the highest and lowest value for the price, quantity demanded and quantity supply
-def setMinAndMaxValues(key, data):
+def set_min_and_max_values(key, data):
     try:
         if (data[key]["all"]):
             data[key]["lowest"] = min(data[key]["all"])
@@ -38,23 +38,23 @@ def setMinAndMaxValues(key, data):
     return data
 
 # Set data for each row
-def setDataByRow(item, value, data):
+def set_data_by_row(item, value, data):
     if item.isdecimal():
         data[value]["all"].append(int(item))
     return data
 
 # Fill and sort data inside the dictionnary
-def fillData(row, data):
+def fill_data(row, data):
     keys = { "price": 0, "quantityDemanded": 1, "quantitySupply": 2 }
     for (key, value) in keys.items():
-        data = setDataByRow(row[value], key, data)
+        data = set_data_by_row(row[value], key, data)
     if data["equilibriumPrice"] == None:
-        data = setEquilibrium(data, row[1], row[2], row[0])
+        data = set_equilibrium(data, row[1], row[2], row[0])
     del keys
     return data
 
 # Clean each row of any whitespace
-def cleanRow(row):
+def clean_row(row):
     newRow = []
     for item in row:
         item = item.strip()
@@ -63,7 +63,7 @@ def cleanRow(row):
     return (newRow)
 
 # Initialization of the dictionary which will contain all the results
-def initData():
+def init_data():
     data = {
         "price": {
             "lowest": None,
@@ -86,19 +86,19 @@ def initData():
     return (data)
 
 # Open and read the data inside the CSV file
-def getData(file):
-    data = initData()
+def get_data(file):
+    data = init_data()
     with open(file, newline='') as csvfile:
         reader = csv.reader(csvfile, delimiter=',', quoting=csv.QUOTE_NONE)
         for row in reader:
-            row = cleanRow(row)
-            data = fillData(row, data)
+            row = clean_row(row)
+            data = fill_data(row, data)
     for key in data.keys():
-        data = setMinAndMaxValues(key, data)
+        data = set_min_and_max_values(key, data)
     return data
 
 # Display the founded results in the console
-def displayResults(data):
+def display_results(data):
     print("The equilibrium price is equal to {} and the equilibrium quantity is equal to {}.".format(data["equilibriumPrice"], data["equilibriumQuantity"]))
     print("The minimum price is {}$ and the maximum price is {}$.".format(data["price"]["lowest"], data["price"]["highest"]))
     print("The lowest quantity demanded is equal to {} and the highest quantity demanded is equal to {}.".format(data["quantityDemanded"]["lowest"], data["quantityDemanded"]["highest"]))
@@ -106,14 +106,14 @@ def displayResults(data):
 
 # Build the final graph and display the results
 def builder(file):
-    data = getData(file)
+    data = get_data(file)
     plt.plot(data["quantityDemanded"]["all"],data["price"]["all"])
     plt.plot(data["quantitySupply"]["all"], data["price"]["all"])
     plt.legend(["Demand","Supply"])
     plt.ylabel("Price")
     plt.xlabel("Supply and Demand Quantity")
     plt.suptitle("Demand and Supply schedule")
-    displayResults(data)
+    display_results(data)
     plt.show()
     return
 
@@ -125,7 +125,7 @@ def cli():
 @click.argument('file')
 def buildGraphic(file):
     """Build Supply and Demand curve into a graph"""
-    if isCSV(file):
+    if is_CSV(file):
         builder(file)
     return
 
